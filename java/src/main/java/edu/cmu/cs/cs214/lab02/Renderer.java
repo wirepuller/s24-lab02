596112ea930138315d9d93366cabf5721bfe5e94
wirepuller <zhihaozhang@LAPTOP-2JLTR0HU>
2024-01-20 21:12:43 -0500

    17514-lab2-ver.1 Refactor shape classes for encapsulation and interface implementation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,17 +3,15 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
+    private Shape shape;
     
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    public Renderer(Shape shape) {
+        this.shape = shape;
     }
 
-    void draw() {
-        double area = rectangle.getArea();
-
+    public void draw() {
+        double area = shape.getArea();
         // assume implementation
-
         System.out.println("Shape printed\n" + "Its area is " + area);
     }
 }
