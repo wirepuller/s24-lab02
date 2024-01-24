85d1f643379bc75edf5a9ff09d11624f795bac25
wirepuller <zhihaozhang@LAPTOP-2JLTR0HU>
2024-01-24 00:35:15 -0500

    lab2 commit

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -4,9 +4,9 @@ import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Shape rectangle = new Rectangle(2, 3);
+        Shape shape = new Rectangle(2, 3);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer = new Renderer(shape);
 
         renderer.draw();
     }
